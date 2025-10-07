cdb33619c20221da2412843f1e11a1374a5e3d0e
Elsa <zeyugao@outlook.com>
2025-10-07 19:56:37 +0800

    Revert "some stage"
    This reverts commit 1e7f124ff54a3a21a4b73cc4f98a2cb8b45c2967.

--- QCloudMusicApi/module.cpp
+++ QCloudMusicApi/module.cpp
@@ -735,7 +735,6 @@ QVariantMap Api::cloud(QVariantMap query) {
         songFile["size"] = data.length();
         query["songFile"] = songFile;
     }
-    query["domain"] = "https://interface.music.163.com";
     const QVariantMap res = request(
         "/api/cloud/upload/check",
         {
@@ -762,7 +761,6 @@ QVariantMap Api::cloud(QVariantMap query) {
         songName = query["songName"].toString();
     }
 
-    query["domain"] = "https://music.163.com";
     const auto tokenRes = request(
         "/api/nos/token/alloc",
         {
@@ -794,7 +792,6 @@ QVariantMap Api::cloud(QVariantMap query) {
             };
         }
     }
-    query["domain"] = "https://music.163.com";
     const auto res2 = request(
         "/api/upload/cloud/info/v2",
         {
@@ -817,7 +814,6 @@ QVariantMap Api::cloud(QVariantMap query) {
         };
     }
 
-    query["domain"] = "https://interface.music.163.com";
     const auto res3 = request(
         "/api/cloud/pub/v2",
         {
